77a84e924107b1de3c97621005a6d92c976fe397
Chan Chun Yip Roy <roynchan@Chans-MacBook-Pro.local>
2021-12-09 23:32:06 -0500

    adding basic codeing

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-#a list of marvel characters
-list1 = ["IronMan", "Spiderman", "CaptinAmerica" ]
-print(list1)
+from gamecomponents import gamevar
+import random 
+guess = ""
+answer = random.choice(gamevar.list1)
+print(answer)
+quetionSpider = random.choice(gamevar.listspider)
+quetioniron = random.choice(gamevar.listiron)
+quetioncap = random.choice(gamevar.listcap)
+lose = False
+while gamevar.player == False and gamevar.playerLives > 0 :
+    if answer == "Spider-Man" :
+            print(quetionSpider)
+            gamevar.player = input("Enter Your Guess! ")
+           
+
+    if answer == "Iron-Man" :
+        print(quetioniron)
+        gamevar.player = input("Enter Your Guess! ")
+    
+
+    if answer == "Captin-America" :
+        print(quetioncap)
+        gamevar.player = input("Enter Your Guess! ")
+     
+
+    if gamevar.player == answer :
+        gamevar.player = True
+        print("You get it ! It is"+ answer)
+    elif  gamevar.player != answer :
+        print("unlucky ! lets try again ")
+        gamevar.playerLives = gamevar.playerLives - 1
+    else : print("Out of guessing chance, You Lose")
+
